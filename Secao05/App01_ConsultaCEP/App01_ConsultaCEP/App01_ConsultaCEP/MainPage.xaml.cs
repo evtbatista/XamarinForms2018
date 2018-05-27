42ae3dc776cde0830126e2b35fb7db9af5b4b493
everton.batista <everton.batista@PC-EVERTON>
2018-05-27 18:50:14 -0300

    commit

---
+++ Secao05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using App01_ConsultaCEP.Modelo;
+using App01_ConsultaCEP.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_ConsultaCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BTNBUSCAR.Clicked += BuscarCEP;
+		}
+
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = CEP.Text.Trim();
+
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = $"Endereço: {end.Logradouro} -  {end.Complemento} - {end.Bairro} - {end.UF}";
+        }
+    }
+}
---
+++ Secao05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultaCEP.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        //private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string EnderecoURL = $"http://viacep.com.br/ws/{cep}/json/";
+
+            WebClient webClient = new WebClient();
+            string resultado = webClient.DownloadString(EnderecoURL);
+
+            Endereco endereco = JsonConvert.DeserializeObject<Endereco>(resultado);
+
+            return endereco;
+        }
+            
+    }
+}
